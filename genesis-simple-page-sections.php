e7892a5fbfcd5060231608ab49ec1b61f4b28205
Doug Yuen <Doug Yuen@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-19 03:59:44 +0000

    First commit
    git-svn-id: https://plugins.svn.wordpress.org/genesis-simple-page-sections/trunk@841150 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ genesis-simple-page-sections.php
@@ -0,0 +1,80 @@
+<?php
+/*
+Plugin Name: Genesis Simple Page Sections
+Plugin URI: http://efficientwp.com/genesis-simple-page-sections
+Description: Easily make full width page sections in Genesis. Must be using the Genesis theme framework.
+Version: 0.1
+Author: Doug Yuen
+Author URI: http://efficientwp.com
+License: GPLv2
+*/
+
+define('GSPS_PLUGIN_PATH_FOR_SUBDIRS', plugins_url(str_replace(dirname(dirname(__FILE__)), '', dirname(__FILE__))));
+register_activation_hook( __FILE__, array( $this, 'activation_hook' ) );
+
+add_shortcode( 'genesis-simple-page-section', 'ewp_gsps' );
+wp_enqueue_style( 'genesis-simple-page-sections-styles', GSPS_PLUGIN_PATH_FOR_SUBDIRS . '/includes/styles.css'  );
+
+add_filter( 'body_class', 'ewp_gsps_body_class');
+
+function ewp_gsps_body_class( $classes ) {
+	global $post;
+     if( has_shortcode( $post->post_content, 'genesis-simple-page-section' ) ) {
+     	$classes[] = 'genesis-simple-page-sections';
+     }
+     return $classes;
+}
+	
+function ewp_gsps( $atts, $content = null ) {
+	extract( shortcode_atts( array(
+		'outer_css' => '',
+		'inner_css' => '',
+		'color' => '',
+		'width' => ''
+		), $atts ) );
+	$flat_colors = array( 
+		'turquoise', 'green sea', 'emerald', 'nephritis', 
+		'peter river', 'belize hole', 'amethyst', 'wisteria', 
+		'wet asphalt', 'midnight blue', 'sun flower', 'orange', 
+		'carrot', 'pumpkin', 'alizarin', 'pomegranate', 
+		'clouds', 'silver', 'concrete', 'asbestos' 
+	);
+	$flat_color_values = array( 
+		'#1abc9c', '#16a085', '#2ecc71', '#27ae60', 
+		'#3498db', '#2980b9', '#9b59b6', '#8e44ad', 
+		'#34495e', '#2c3e50', '#f1c40f', '#f39c12', 
+		'#e67e22', '#d35400', '#e74c3c', '#c0392b', 
+		'#ecf0f1', '#bdc3c7', '#95a5a6', '#7f8c8d' 
+	);
+	$background_color = esc_attr( $color );
+	$width = esc_attr( $width );
+	$background_color = str_replace( $flat_colors, $flat_color_values, $background_color );
+	$output = '';
+	$output .= '<div class="gsps-outer" style="';
+	if ( $background_color ) {
+		$output .= 'background-color:';
+		if ( ctype_xdigit( $background_color ) ) {
+			$output .= '#';
+		}
+		$output .= $background_color . ';';
+	}
+	if ( esc_attr( $outer_css ) ) {
+		$output .= esc_attr( $outer_css );
+	}
+	$output .= '">';
+	$output .= '<div class="gsps-inner" style="';
+	if ( $width ) {
+		$output .= 'max-width:' . $width;
+		if ( ctype_digit( $width ) ) {
+			$output .= 'px';
+		}
+		$output .= ';';
+	}
+	if ( esc_attr( $inner_css ) ) {
+		$output .= esc_attr( $inner_css );
+	}
+	$output .= '">'  . do_shortcode($content) . '</div></div>';
+	return $output;
+}
+
+?>
---
+++ readme.txt
@@ -0,0 +1,50 @@
+=== Genesis Simple Page Sections ===
+
+Contributors: Doug Yuen
+Author URI: http://efficientwp.com
+Plugin URI: http://efficientwp.com/genesis-simple-page-sections
+Tags: genesis, genesiswp, studiopress
+Requires at least: 3.0
+Tested up to: 3.8
+Stable tag: 0.1
+License: GPLv2 or later
+
+Easily make full width page sections in Genesis. Must be using the Genesis theme framework.
+
+== Description ==
+
+Easily make full width page sections in Genesis via shortcode. Must be using the [Genesis theme framework](http://www.shareasale.com/r.cfm?B=386922&U=488446&M=28169&urllink= "Genesis theme framework").
+
+Basic shortcode syntax:
+
+\[genesis-simple-page-section color="orange" width="960" outer_css="" inner_css=""\]
+
+\[/genesis-simple-page-section\]
+
+For more about the shortcode syntax, available parameters, and color presets, please visit the plugin page: [http://efficientwp.com/plugins/genesis-simple-page-sections](http://efficientwp.com/plugins/genesis-simple-page-sections= "Genesis Simple Page Sections").
+
+Created by [EfficientWP](http://efficientwp.com= "EfficientWP").
+
+== Installation ==
+
+1. Upload `genesis-simple-page-sections.zip` into your plugin directory (typically `/wp-content/plugins/`).
+2. Unzip the `genesis-simple-page-sections.zip` file.
+3. Activate the plugin through the 'Plugins' menu in WordPress.
+
+== Frequently Asked Questions ==
+
+Plugin page: [http://efficientwp.com/plugins/genesis-simple-page-sections](http://efficientwp.com/plugins/genesis-simple-page-sections= "Genesis Simple Page Sections").
+
+== Screenshots ==
+
+1. An example of the plugin in use on http://efficientwp.com (using the Dynamik child theme).
+
+== Changelog ==
+
+= 0.1 =
+* Initial release
+
+== Upgrade Notice ==
+
+Coming soon.
+
